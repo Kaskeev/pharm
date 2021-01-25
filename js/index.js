19e0df759f3a3c465947339c463c766cbe339fd9
Kaskeev <62796577+Kaskeev@users.noreply.github.com>
2021-01-26 01:41:15 +0600

    meked desktop header

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){ 
+    PopUpHide();
+});
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+};
+$(document).ready(function(){
+    $(".header__burger").click(function(event){
+        $(".header__burger, .header__top-nav").toggleClass('active');
+    });
+});
